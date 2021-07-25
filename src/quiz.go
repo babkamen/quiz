9f67ff76952c8cefd96d57128f2baf88afcfc376
Roman Lukash <roman.lukash@groupm.tech>
2021-07-25 22:05:24 +0300

    Finished implementation

--- src/quiz.go
+++ src/quiz.go
@@ -1,50 +1,112 @@
 package main
 
 import (
+	"bufio"
 	"encoding/csv"
+	"flag"
 	"fmt"
+	"github.com/hako/durafmt"
 	"log"
 	"os"
 	"strings"
+	"time"
 )
 
+type QuizResult struct {
+	totalQuestions int
+	correctAnswers int
+}
+
+var quizTime time.Duration
+
+func init() {
+	flag.DurationVar(&quizTime, "quiz-time", 30*time.Second, "quiz total time")
+	flag.Parse()
+}
+
 func main() {
+
+	records := readProblemsFile()
+
+	quizResult := QuizResult{totalQuestions: len(records)}
+
+	startQuiz(quizResult.totalQuestions)
+
+	timer := time.NewTimer(quizTime)
+	go func(quizResult *QuizResult) {
+		<-timer.C
+		quizResult.printResults()
+		os.Exit(0)
+	}(&quizResult)
+
+	processQuiz(records, &quizResult)
+
+	quizResult.printResults()
+}
+
+func startQuiz(totalQuestions int) {
+	duration, err := durafmt.ParseString(quizTime.String())
+	if err != nil {
+		log.Fatal("Something went wrong when parsing duration ", err)
+	}
+	fmt.Printf("Quiz contains %v questions. You have %v to answer them\nPress enter to start quiz\n",
+		totalQuestions, duration)
+	_, err = bufio.NewReader(os.Stdin).ReadBytes('\n')
+	if err != nil {
+		log.Fatal("Something went wrong when reading input ", err)
+	}
+}
+
+func readProblemsFile() [][]string {
 	file, err := os.Open("problems.csv")
 	if err != nil {
-		log.Fatal("Something went wrong when opening the file", err)
-		os.Exit(1)
+		log.Fatal("Something went wrong when opening the file ", err)
 	}
 
 	r := csv.NewReader(file)
 	records, err := r.ReadAll()
 
 	if err != nil {
-		log.Fatal("Something went wrong when reading csv", err)
-		os.Exit(1)
+		log.Fatal("Something went wrong when reading csv ", err)
+	}
+	//validate file
+	for i, r := range records {
+		if len(r) != 2 {
+			log.Fatalf("Expected csv rows to have 2 values but got %v on line %v ", r, i+1)
+		}
 	}
 
-	total := len(records)
-	var correctCount int
+	return records
+}
 
-	for _, r := range records {
-		if len(r) != 2 {
-			log.Fatal("Expected csv rows to have 2 values but got ", r)
-			os.Exit(1)
-		}
+func processQuiz(records [][]string, quizResult *QuizResult) {
+	for i, r := range records {
 		exercise := r[0]
 		correctAnswer := r[1]
 		if strings.Contains(exercise, "?") {
 			fmt.Println(exercise)
 		} else {
-			fmt.Printf("what %v, sir?\n", exercise)
+			fmt.Printf("%v. what %v, sir?\n", i+1, exercise)
 		}
-		var answer string
-
-		// Taking input from user
-		fmt.Scanln(&answer)
-		if answer == correctAnswer {
-			correctCount++
+		answer := getInputFromUser()
+		if strings.EqualFold(answer, correctAnswer) {
+			quizResult.correctAnswers++
 		}
 	}
-	fmt.Printf("Correct answers %v out of  %v\n", correctCount, total)
+}
+
+func (quizResult QuizResult) printResults() {
+	fmt.Printf("Correct answers %v out of  %v\n", quizResult.correctAnswers, quizResult.totalQuestions)
+}
+
+func getInputFromUser() string {
+	var result string
+	reader := bufio.NewReader(os.Stdin)
+	result, err := reader.ReadString('\n')
+	if err != nil {
+		log.Fatal("Something went wrong when reading input ", err)
+	}
+	result = strings.TrimSpace(result)
+	fmt.Printf("User entered %q\n", result)
+	return result
 }
